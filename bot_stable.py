2d99cb3012c3e3c08e533a5a7ab1ce355b8cd5fa
Osif <osif@slh.local>
2026-07-06 18:40:18 +0300

    Full SLH OS restored + staking module integrated correctly

--- bot_stable.py
+++ bot_stable.py
@@ -51,6 +51,7 @@ import sandbox_handler
 import myprogress_handler
 import help_handler
 import broadcast_handler
+import staking_handler
 import refresh_token_handler
 import smart_leaderboard
 
@@ -181,6 +182,7 @@ course_handlers.register_course_handlers(bot)
 learn_handlers.register(bot)
 welcome_handler.init(bot)
 broadcast_handler.init(bot)
+staking_handler.init(bot)
 project_commands.register(bot)
 monitor_handler.init(bot)
 ask_handler.init(bot)
