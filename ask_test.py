ead23b3930f833bcdd17392b594e7130896d9f7c
OsifDev <osif.erez.ungar@gmail.com>
2026-08-06 00:18:10 +0300

    remove legacy ask handler and remove exposed secrets

---
+++ ask_test.py
@@ -0,0 +1,31 @@
+﻿from handlers.advanced_ask_handler import register_ask_handler
+
+class B:
+    def message_handler(self, **kwargs):
+        def wrap(fn):
+            self.fn = fn
+            print("REGISTERED", fn.__name__)
+            return fn
+        return wrap
+
+    def reply_to(self,msg,text,**kwargs):
+        print("REPLY:",repr(text))
+
+b=B()
+
+print("BEFORE REGISTER")
+register_ask_handler(b)
+print("AFTER REGISTER")
+
+class U:
+    id=8789977826
+    is_bot=False
+
+class M:
+    from_user=U()
+    text="/ask test"
+
+print("BEFORE CALL")
+b.fn(M())
+print("AFTER CALL")
+
