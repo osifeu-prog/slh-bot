42f898c70ba69651c6150a9da41c802d87a01030
OsifDev <osif.erez.ungar@gmail.com>
2026-08-07 22:43:38 +0300

    feat: add /exec command, esp uses devices.json

--- handlers/loader.py
+++ handlers/loader.py
@@ -1,4 +1,4 @@
-def load_handlers(bot, context):
+﻿def load_handlers(bot, context):
     from handlers.logo_handler import register_logo_handler
     register_logo_handler(bot)
 
@@ -96,5 +96,6 @@ def load_handlers(bot, context):
 
 
     print("? ALL HANDLERS READY")
+
 
 
