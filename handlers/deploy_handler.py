943efd2ce55933058d960f6236a49ce1e50dfaf2
Osif ungar <osif.e.u@gmail.com>
2026-08-23 00:04:50 +0300

    use deploy instead of redeploy

--- handlers/deploy_handler.py
+++ handlers/deploy_handler.py
@@ -24,7 +24,7 @@ def register(bot):
 
         query = """
         mutation {
-          serviceInstanceRedeploy(
+          serviceInstanceDeploy(
             serviceId: "13d97581-0199-4f6a-80d1-885c9304ffc5",
             environmentId: "661caa13-83cb-4197-8825-943bebf96c5a"
           )
