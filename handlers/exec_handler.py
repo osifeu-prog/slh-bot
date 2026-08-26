fbc9a35eb7d02005cba2f4275ac73a190645725c
Osif ungar <osif.e.u@gmail.com>
2026-08-26 21:09:55 +0300

    alpha security + terms + backup + db updates

--- handlers/exec_handler.py
+++ handlers/exec_handler.py
@@ -1,4 +1,37 @@
-﻿import subprocess
+
+LAST_EXEC = {}
+
+def rate_limit(user_id):
+    import time
+    now = time.time()
+    if user_id in LAST_EXEC and now - LAST_EXEC[user_id] < 2:
+        return False
+    LAST_EXEC[user_id] = now
+    return True
+
+
+import json, time
+def audit_exec(user_id, cmd):
+    log = json.load(open("state/exec_audit.json"))
+    log.append({
+        "user": user_id,
+        "cmd": cmd,
+        "time": time.time()
+    })
+    open("state/exec_audit.json","w").write(json.dumps(log,indent=2))
+
+
+SAFE_COMMANDS = [
+    'ls', 'cat', 'grep', 'python3', 'echo', 'head', 'tail'
+]
+
+def is_safe(cmd):
+    for bad in ['rm', 'mv', 'chmod', 'chown', 'curl http', 'wget', 'scp', 'dd', 'kill', 'pkill']:
+        if bad in cmd:
+            return False
+    return True
+
+import subprocess
 import os
 from core.progress_tracker import progress_report
 
