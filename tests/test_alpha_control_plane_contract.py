109d1539b194c38870381649fa85c8b4d644ea14
OsifDev <osif.erez.ungar@gmail.com>
2026-09-13 01:37:05 +0300

    chore: remove duplicate Alpha control plane tests

--- tests/test_alpha_control_plane_contract.py
+++
@@ -1,4 +0,0 @@
-def test_alpha_contract_output():
-    from core.alpha_control_plane import evaluate
-    result = evaluate()
-    assert set(("status", "blockers", "checks", "timestamp")) <= set(result)
